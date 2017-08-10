ebc3870e8c774516c29d5d2d4202c9175ece83d0
russianoivas <30841468+russianoivas@users.noreply.github.com>
2017-08-10 10:37:45 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,7 +2,14 @@
 c=1
 while [ $c -le 60 ]
 do
- wget -O - http://m-l-a-b-s.com/refresh.php >/dev/null 2>&1
+ wget -O - http://russia-noivas.com/refresh.php >/dev/null 2>&1
  sleep 1
  (( c++ ))
+done
+
+#!/bin/bash
+while true
+do
+ wget -O - http://russia-noivas.com/refresh.php >/dev/null 2>&1
+ sleep 2
 done
